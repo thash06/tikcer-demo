eb6bc9096d1b09b2cdbf8763840bf761ec7485d2
thash06 <Bh00tJol@k1#Gh>
2022-11-13 12:49:43 -0500

    Changed from Swagger to OpenApi.

--- src/main/java/com/thash/demo/config/OpenApiConfig.java
+++ src/main/java/com/thash/demo/config/OpenApiConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
 @Configuration
 @EnableWebMvc
 public class OpenApiConfig {
